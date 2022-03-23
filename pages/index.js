7154d724a4bbd57d7b0b4c46655e098a3cf5eb19
n1rium <blomgrenjohnny@gmail.com>
2022-03-23 23:02:57 +0100

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
 
 export default function Home() {
+  const [chat, setChat] = useState([]);
+  const [yd, setYdoc] = useState(null);
+  const [msg, setMsg] = useState("");
+  useEffect(() => {
+    const ydoc = new Y.Doc();
+    const provider = new WebrtcProvider("your-room-name", ydoc, {
+      password: "optional-room-password",
+    });
+    const ymap = ydoc.getMap("state", Y.Array);
+    ymap.observe(() => {
+      console.log("change");
+      setChat([...ymap.get("state")]);
+    });
+    setYdoc(ydoc);
+  }, []);
+
+  const send = () => {
+    console.log(yd);
+    const s = yd.getMap("state");
+    s.set("state", [...chat, msg]);
+  };
+
+  console.log(chat);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,59 +38,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <input type="text" value={msg} onChange={(e) => setMsg(e.target.value)} />
+      <button onClick={send}>Send</button>
+      {chat.map((msg, i) => (
+        <div key={i}>{msg}</div>
+      ))}
     </div>
-  )
+  );
 }
